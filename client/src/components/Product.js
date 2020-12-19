5abfc8c290ceb023d665b1a05f198d19ed6c115c
bwalborn <walbornbryce@gmail.com>
2020-12-19 13:38:54 -0600

    rating.js/components and styling in index.css

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import Products from '../products';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -14,9 +15,7 @@ const Product = ({ product }) => {
             <Card.Title as='div' ><strong>{product.name}</strong></Card.Title>
         </a>
       <Card.Text as='div'>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
       </Card.Text>
       <Card.Text as='h3'>
           ${product.price}
