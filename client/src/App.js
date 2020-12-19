95b92ef8bdf995d8faf59f048ecd968a412d0d9b
bwalborn <walbornbryce@gmail.com>
2020-12-19 16:48:45 -0600

    product.js design and react-router-dom
    changed href tags to approprate Link tags. Developed product view page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeView from './views/HomeView';
+import ProductView from './views/ProductView';
+
 
 
 const App = () => {
   return (
-    <>
-    <Header />
-      <main className='py-3'>
-          <Container>
-            <HomeView />
-          </Container>
-      </main>
-    <Footer />
-    </>
+    <Router>
+      <Header />
+        <main className='py-3'>
+            <Container>
+              <Route path='/' component={HomeView} exact />
+              <Route path='/product/:id' component={ProductView} />
+
+            </Container>
+        </main>
+      <Footer />
+    </Router>
+    
   );
 }
 
