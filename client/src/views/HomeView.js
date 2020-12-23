7687dccadd6cb34601efe652c812b882a415ff1b
bwalborn <walbornbryce@gmail.com>
2020-12-22 22:16:36 -0600

    message and loader components

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -2,24 +2,31 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { listProducts } from '../actions/productActions';
 // import axios from 'axios';
 
 const HomeView = () => {
   const dispatch = useDispatch();
 
+
+  // second #2 -> selecting components from the global state
   const productList = useSelector(state => state.productList);
   const { loading, error, products } = productList;
 
+  // first #1  hooks -> fire off the action to get the products and send through the reducer down into the state.
 useEffect(() => {
   dispatch(listProducts())
 }, [dispatch]) // <= array of dependencies (anything that you want useEffect to fire off then they change)
 
 
+
+// third #3 -> display elements form the state below
   return (
     <>
         <h1>Latest Products</h1>
-        {loading ?  <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
+        {loading ?  <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
         <Row>
             {products.map(product => {
                 return (
