df44f9a450086e29e68089404ebbc3c8cc36f50f
bwalborn <walbornbryce@gmail.com>
2020-12-22 20:56:48 -0600

    redux state into homeview.js

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
+// import axios from 'axios';
 
 const HomeView = () => {
-  const [Products, setProducts] = useState([]);
-  
+  const dispatch = useDispatch();
+
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
+
 useEffect(() => {
-  const fetchProducts = async () => {
-    const { data } = await axios.get('/api/products');
-
-    setProducts(data);
-  }
-
-  fetchProducts();
-}, []) // <= array of dependencies (anything that you want useEffect to fire off then they change)
+  dispatch(listProducts())
+}, [dispatch]) // <= array of dependencies (anything that you want useEffect to fire off then they change)
 
 
   return (
     <>
         <h1>Latest Products</h1>
+        {loading ?  <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
         <Row>
-            {Products.map(product => {
+            {products.map(product => {
                 return (
                 <Col key={product._id} sm={12} md={6} lg={4}>
                     <Product product={product} />
                 </Col>
                 )
             })}
-        </Row>
+        </Row>}
     </>
   );
 }
