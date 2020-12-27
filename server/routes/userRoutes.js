581abb63df7d99c5a841d14af4926eaf387af83d
bwalborn <walbornbryce@gmail.com>
2020-12-27 12:23:24 -0600

    user registration and password encryption

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
 import  { protect } from '../middleware/authMiddleware.js';
 
 // ---- ROUTES: pointing to specific controller functions ----
 
 // router.get('/', getProducts)
 // or
+router.route('/').post(registerUser);
 
 router.post('/login', authUser);
 
