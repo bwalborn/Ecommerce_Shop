9f90eaa820adf44480704a65e0a64972d0ac62aa
bwalborn <walbornbryce@gmail.com>
2020-12-28 20:43:42 -0600

    update profile endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
 import  { protect } from '../middleware/authMiddleware.js';
 
 // ---- ROUTES: pointing to specific controller functions ----
@@ -11,7 +11,7 @@ router.route('/').post(registerUser);
 
 router.post('/login', authUser);
 
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 
 
