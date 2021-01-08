60d18c77aa227f67998700e45ee6f9459a5add9b
bwalborn <walbornbryce@gmail.com>
2021-01-08 17:23:24 -0600

    place order view

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import RegisterView from './views/RegisterView';
 import ProfileView from './views/ProfileView';
 import ShippingView from './views/ShippingView';
 import PaymentView from './views/PaymentView';
+import PlaceOrderView from './views/PlaceOrderView';
 
 
 
@@ -25,6 +26,7 @@ const App = () => {
             <Container>
               <Route path='/shipping' component={ShippingView} />
               <Route path='/payment' component={PaymentView} />
+              <Route path='/placeorder' component={PlaceOrderView} />
               <Route path='/login' component={LoginView} />
               <Route path='/register' component={RegisterView} />
               <Route path='/profile' component={ProfileView} />
