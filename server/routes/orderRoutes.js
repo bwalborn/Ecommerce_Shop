8b2ce605581140a11523e04dca38d19b72ffb97e
bwalborn <walbornbryce@gmail.com>
2021-01-08 20:49:49 -0600

    get order by id endpoint

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import  { protect } from '../middleware/authMiddleware.js';
 
 // ---- ROUTES: pointing to specific controller functions ----
@@ -8,6 +8,7 @@ import  { protect } from '../middleware/authMiddleware.js';
 // router.get('/', getProducts)
 // or
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 
 export default router;
